f43e5f2bf8cafaa495b0eb607cb62c3844ecdf65
xKnriya <knriya@gmail.com>
2025-05-24 18:35:45 -0300

    idk something about cat's module

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,7 +3,6 @@ import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 import { Cat } from './entities/cat.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { Breed } from 'src/breeds/entities/breed.entity';
 import { BreedsModule } from 'src/breeds/breeds.module';
 
 @Module({
